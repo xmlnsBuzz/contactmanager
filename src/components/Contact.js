cc3202f6b3d1e8ebdd9a28bb1e1b73e62d7bc5b5
xmlnsBuzz <html5conq@gmail.com>
2020-01-07 21:23:26 +0900

    Chapter 4 closed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
 
@@ -8,37 +9,41 @@ class Contact extends Component {
    };
    // contact info 가 display: none 상태에서 문서 load
 
-   onDeleteClick = () => {
-      this.props.deleteClickHandler();
+   onDeleteClick = (id, dispatch) => {
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
    }
 
-
-
    render() {
-      const { name, email, phone } = this.props.contact;
+      const { id, name, email, phone } = this.props.contact;
       const { showContactInfo } = this.state;
       // destructuring
       return (
-         <div className="card card-body mb-3">
-            <h4>
-               <i
-                  onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
-                  className="fa fa-chevron-down"
-                  style={{ cursor: 'pointer', marginLeft: '5px' }}
-               />
-               <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-               onClick={this.onDeleteClick}
-               />
-               {` ${name} `}
-            </h4>
-            {showContactInfo ? (
-               <ul className="list-group">
-                  <li className="list-group-item">Email : {email} </li>
-                  <li className="list-group-item">Phone : {phone} </li>
-               </ul>
-            ) : null}
-         </div>
-
+         <Consumer>
+            {value => {
+               const { dispatch } = value;
+               return (
+                  <div className="card card-body mb-3">
+                     <h4>
+                        {` ${name} `}
+                        <i
+                           onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
+                           className="fa fa-chevron-down"
+                           style={{ cursor: 'pointer', marginLeft: '5px' }}
+                        />
+                        <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                           onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                        />
+                     </h4>
+                     {showContactInfo ? (
+                        <ul className="list-group">
+                           <li className="list-group-item">Email : {email} </li>
+                           <li className="list-group-item">Phone : {phone} </li>
+                        </ul>
+                     ) : null}
+                  </div>
+               )
+            }}
+         </Consumer>
       );
    }
 }
@@ -46,7 +51,6 @@ class Contact extends Component {
 
 Contact.propTypes = {
    contact: PropTypes.object.isRequired,
-   deleteClickHandler: PropTypes.func.isRequired
 
 }
 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,13 +3,6 @@ import Contact from './Contact';
 import { Consumer } from '../context';
 
 class Contacts extends Component {
-   deleteContact = id => {
-      const { contacts } = this.state;
-      const newContacts = contacts.filter(contact => contact.id !== id);
-      this.setState({
-         contacts: newContacts
-      });
-   };
 
    render() {
       return (
@@ -24,7 +17,6 @@ class Contacts extends Component {
                            <Contact
                               key={contact.id}
                               contact={contact}
-                              deleteClickHandler={this.deleteContact.bind(this, contact.id)}
                            />
                         ))}
                      </div>
